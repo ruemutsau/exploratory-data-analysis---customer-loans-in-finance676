e72e227017d7e1f96cab461ec403fa892319fd25
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2023-12-10 17:34:04 +0000

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,1 +1,24 @@
+import  RDSDatabase
 
+def connect_to_db():
+    connection =  RDSDatabase.connect(
+        database="database_name",
+        user="username",
+        password="password",
+        host="localhost"
+    return connection
+
+def extract_data():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM table_name")
+    data = cursor.fetchall()
+
+    connection.close()
+    return data
+
+if __name__ == "__main__":
+    data = extract_data()
+    for row in data:
+        print(row)
