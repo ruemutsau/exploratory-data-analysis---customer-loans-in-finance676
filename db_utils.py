68e5e7b88fd1745baeedc560b5e6e0253413cea8
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2023-12-10 17:40:52 +0000

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,11 +1,11 @@
-import  RDSDatabase
+import  customerloans
 
 def connect_to_db():
-    connection =  RDSDatabase.connect(
+    connection =  customerloans.connect(
         database="database_name",
         user="username",
         password="password",
-        host="localhost"
+        host="localhost")
     return connection
 
 def extract_data():
@@ -22,3 +22,27 @@ if __name__ == "__main__":
     data = extract_data()
     for row in data:
         print(row)
+class RDSDatabaseConnector:
+    def __init__(self, database_name, user, password, host):
+        self.connection = customerloans.connect(
+            database=database_name,
+            user=user,
+            password=password,
+            host=host)
+        
+    def extract_data(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        return data
+
+    def close_connection(self):
+        self.connection.close()
+
+if __name__ == "__main__":
+    connector = RDSDatabaseConnector("database_name", "username", "password", "localhost")
+    data = connector.extract_data("SELECT * FROM table_name")
+    for row in data:
+        print(row)
+
+    connector.close_connection()
