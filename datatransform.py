7c3bbcbda76b251a1940ac3c838d8d2683b3fd8b
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2024-01-17 10:13:03 +0000

    Update datatransform.py

--- datatransform.py
+++ datatransform.py
@@ -8,9 +8,15 @@ The data's columns can be transformed using this class.
         self.column_name = column_name
       def extract_integer_from_string(self):
         # Extract integers from the column
-        self.DataFrame[self.column_name] = self.DataFrame[self.column_name].str.extract('(\d+)').astype(int)
+        self.DataFrame[self.column_name] = self.DataFrame[self.column_name].str.extract('(\d+)').astype('Int32')
         return self.DataFrame
+def replace_string_text(self, DataFrame: pd.DataFrame, column_name: str, original_string: str, new_string: str)
+DataFrame[column_name] = DataFrame[column_name].str.replace(original_string, new_string)
+        return DataFrame
 
+    def convert_string_to_date(self, DataFrame: pd.DataFrame, column_name: str)
+ DataFrame[column_name] = pd.to_datetime(DataFrame[column_name], errors='coerce').dt.to_period('M')
+        return DataFrame
 
 
   
