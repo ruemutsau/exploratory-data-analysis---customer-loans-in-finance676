af9a28f0af36fecd3139f1a63a99585b9214ff4a
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2024-01-17 10:03:51 +0000

    Update datatransform.py

--- datatransform.py
+++ datatransform.py
@@ -1,1 +1,16 @@
+import pandas as pd
+...
+The data's columns can be transformed using this class.
+...
+  class DataTransform:
+    def __init__(self, DataFrame, column_name):
+        self.DataFrame = DataFrame
+        self.column_name = column_name
+      def extract_integer_from_string(self):
+        # Extract integers from the column
+        self.DataFrame[self.column_name] = self.DataFrame[self.column_name].str.extract('(\d+)').astype(int)
+        return self.DataFrame
 
+
+
+  
