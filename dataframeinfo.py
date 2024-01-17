a8ddd2426962bb9cd81ad323b8879334cc92870c
ruemutsau <149763297+ruemutsau@users.noreply.github.com>
2024-01-17 11:03:48 +0000

    Update dataframeinfo.py

--- dataframeinfo.py
+++ dataframeinfo.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+class DataFrameInfo:
+   def describe_dtypes(self, DataFrame: pd.DataFrame, column_name: str = None)
+       if column_name is not None: # In the case that a column name IS provided.
+            if column_name not in DataFrame.columns: # In the case the provided column_name is NOT in the DataFrame.
+                raise ValueError(f"Column '{column_name}' not found in the dataframe.")
+            return DataFrame[column_name].dtypes
+        else: 
+            return DataFrame.dtypes
